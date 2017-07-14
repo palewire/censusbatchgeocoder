aff8dec10fa6f2072123cab69ddb2013c6a92840
palewire <ben.welsh@gmail.com>
2017-07-14 13:53:59 -0700

    v0.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='censusbatchgeocoder',
-    version='0.0.6',
+    version='0.0.7',
     description='A simple Python wrapper for U.S. Census Geocoding Services API batch service',
     author='Los Angeles Times Data Desk',
     author_email='datadesk@latimes.com',
